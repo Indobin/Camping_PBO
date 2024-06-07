7f897239e1b6fbef76a3580aff4cbc77d80b6f91
AK4KU <ak4kurniawan@gmail.com>
2024-06-07 16:12:33 +0700

    mengubah profil penyewa

---
+++ Projek_Akhir_PBO/Models/Penyewa/ProfilePy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projek_Akhir_PBO.Models.Penyewa
+{
+    internal class ProfilePy
+    {
+        public int id_penyewa { get; set; }
+        public string nama_penyewa { get; set; }
+        public string no_telepon_penyewa { get; set; }
+        public string alamat_penyewa { get; set; }
+    }
+}
