f8238b904b64bf0d674739cff1f42aa071ff659c
AK4KU <ak4kurniawan@gmail.com>
2024-06-07 21:11:36 +0700

    Add Profile pemilik

---
+++ Projek_Akhir_PBO/Models/Pemilik/ProfilePm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projek_Akhir_PBO.Models.Pemilik
+{
+    internal class ProfilePm
+    {
+        public int id_pemilik { get; set; }
+        public string nama_pemilik { get; set; }
+        public string no_telepon_pemilik { get; set; }
+        public string alamat_pemilik { get; set; }
+    }
+}
